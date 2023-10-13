e768c7691b096ef3dd3d358d2fb9ecd9d2443299
ri0629 <tr08792@gmail.com>
2023-10-13 23:34:41 +0900

    ログアウトの実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  get 'items/index'
   devise_for :users
   get 'messages/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root to: "messages#index"
+  root to: "items#index"
+  
 end
