b6ffc20f9e3ee3d8b012c81fd297308e964bb877
ri0629 <tr08792@gmail.com>
2023-10-27 15:39:25 +0900

    販売利益追加

--- app/javascript/item_profit.js
+++ app/javascript/item_profit.js
@@ -2,8 +2,18 @@ window.addEventListener('turbo:load', () => {
   console.log("OK");
   const priceInput = document.getElementById("item-price");
   console.log(priceInput);
+  
   priceInput.addEventListener("input", () => {
-  console.log("イベント発火");
+    const inputValue = priceInput.value;
+    console.log(inputValue);
+    
 
+    const addTaxDom = document.getElementById("add-tax-price");
+    addTaxDom.innerHTML = Math.floor(priceInput.value * 0.1);
+
+    const profitTaxDom = document.getElementById("profit");
+    profitTaxDom.innerHTML = Math.floor(priceInput.value - Math.floor(priceInput.value * 0.1 ));
+   
+    
 })
 });
