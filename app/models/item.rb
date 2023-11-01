0514150ac2a850676a087f17e1376af990e8e542
ri0629 <tr08792@gmail.com>
2023-11-01 18:26:53 +0900

    テストコード

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,13 +6,13 @@ class Item < ApplicationRecord
   with_options presence: true do
 end
   
-validates :item, presence: true
-validates :price, presence: true
-validates :description, presence: true
+validates :item, presence: true, length: { maximum: 40 }
+validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+validates :description, presence: true, length: { maximum: 1000 }
 validates :category_id, presence: true
-#validates :situation_id, presence: true
-#validates :delivery_charge_id, presence: true
-#validates :prefecture_id, presence: true
-#validates :estimated_day_id, presence: true
+validates :situation_id, presence: true
+validates :delivery_charge_id, presence: true
+validates :prefecture_id, presence: true
+validates :estimated_day_id, presence: true
 
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+  #pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = Factorybot.build(:item)
+  end
+
+  describe '商品登録' do
+    context '商品登録できるとき' do
+    it 'すべて存在すれば登録できる' do
+      expect(@item).to be_valid
+    end
+  end
+
+    
+    context '商品登録できないとき' do
+    it '商品画像が空では登録できない' do
+    end
+  end
+
+    it '商品名が空では登録できない' do
+    end
+    it '商品説明が空では登録できない' do
+    end
+    it 'カテゴリーが空では登録できない' do
+    end
+    it '商品の状態が空では登録できない' do
+    end
+    it '配送料の負担が空では登録できない' do
+    end
+    it '配送元の地域が空では登録できない' do
+    end
+    it '発送までの日数が空では登録できない' do
+    end
+    it '価格が空では登録できない' do
+    end
+  end
+    
+
+
