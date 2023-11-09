8cf6435baebf3cc737a4babb906ef316fd61775f
ri0629 <tr08792@gmail.com>
2023-11-10 00:08:56 +0900

    ルーティング記述

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   devise_for :users
 
   root to: "items#index"
-  resources :items, only: [:index, :new, :create] do
-    get '/furima-footer.png', to: 'controller_name#action_name'
+  resources :items, only: [:index, :new, :create, :show] do
+    get '/furima-footer.png', to: 'items#footer_image'
   end
 
 end
