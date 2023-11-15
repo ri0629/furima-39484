293a8f2b736930e48f254738ed7219961161d1bd
ri0629 <tr08792@gmail.com>
2023-11-15 16:23:09 +0900

    order_form作成

---
+++ app/models/order_form.rb
@@ -0,0 +1,13 @@
+class OrderForm
+  include ActiveModel::Model
+  attr_accessor :token, :user_id, :item_id, :postcode, :prefcture_id, :city, :block, :building, :phone_number
+
+  # ここにバリデーションの処理を書く
+  validates :, presence: true
+
+end
+  def save
+    # 各テーブルにデータを保存する処理を書く
+    orders = Orders.create (user_id: user_id, item_id: item_id)
+  end
+end
