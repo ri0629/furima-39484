d994598f8cc91df223c77431bc76ca14201f2cca
ri0629 <tr08792@gmail.com>
2023-11-15 21:51:02 +0900

    params.require記述

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,15 +1,21 @@
 class OrdersController < ApplicationController
+before_action :authenticate_user!
+
+
    def index
     @address = address.new
    end
 
    def create
-    @address = address.new(order_params)
+    @address = address.new(address_params)
 
     binding.pry
    end
 
+  private
 
+  def address_params
+   params.require(:address).permit(postcode, :prefecture_id, :city, :block, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
 
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   root to: "items#index"
+  resources :orders, only: [:index, :create]
   resources :items do
      get '/furima-footer.png', to: 'items#footer_image',on: :collection
      patch '/:id', to: 'items#update', on: :member
