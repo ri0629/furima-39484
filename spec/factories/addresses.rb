624fb65a8fc206d5160a4b6468a98efd16a1e5ce
ri0629 <tr08792@gmail.com>
2023-11-26 20:13:42 +0900

    削除

--- spec/factories/addresses.rb
+++ spec/factories/addresses.rb
@@ -1,7 +1,6 @@
 FactoryBot.define do
   factory :order_form do
       
-    user_id { Faker::Number.non_zero_digit }
     item_id { Faker::Number.non_zero_digit }
     postcode {"000-0000"}
     prefecture_id {"2"}
