473a7ee2f103d82a265545454538013afc775204
ri0629 <tr08792@gmail.com>
2023-12-01 22:41:57 +0900

    javascript

--- app/javascript/item_profit.js
+++ app/javascript/item_profit.js
@@ -4,13 +4,16 @@ window.addEventListener('turbo:load', () => {
   
   priceInput.addEventListener("input", () => {
     const inputValue = priceInput.value;
-    
+    console.log(inputValue);
     
 
     const addTaxDom = document.getElementById("add-tax-price");
-    addTaxDom.innerHTML = Math.floor(priceInput.value * 0.1);
-
+    addTaxDom.innerHTML = Math.floor(inputValue * 0.1);
+    console.log(addTaxDom);
+    
     const profitTaxDom = document.getElementById("profit");
+    const value_result = inputValue * 0.1
+    console.log(value_result)
     profitTaxDom.innerHTML = Math.floor(priceInput.value - Math.floor(priceInput.value * 0.1 ));
    
     
